35482be623ff830ebc348fb53606a2b762c1ae80
Adriana Cabrera <adriana.cabreradelgado@phsa.ca>
2024-03-11 11:33:15 -0700

    check_metrics qc name update

--- bin/extractSampleIDs.py
+++ bin/extractSampleIDs.py
@@ -7,7 +7,7 @@ def extract_sample_IDs(summary_file):
     with open(summary_file, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            if row['check_metrics'] == 'REVIEW' or row['check_metrics'] == 'FAIL/REVIEW':
+            if row['check_metrics'] == 'REVIEW' or row['check_metrics'] == 'QC_FAIL/REVIEW':
                 extractSamples.append(row['sample_name'])
             else:
                 pass # skip this row
